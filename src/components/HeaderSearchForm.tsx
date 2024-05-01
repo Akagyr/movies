1d406096f63452b0c8486fc27dafaa92978c820a
Alex <casha.mironyuk.2001@gmail.com>
2024-05-01 23:10:53 +0300

    Fixed bug with custom input in header search

--- src/components/HeaderSearchForm.tsx
+++ src/components/HeaderSearchForm.tsx
@@ -4,13 +4,28 @@ export default function HeaderSearchForm() {
   return (
     <form className='hidden lg:flex px-10 col-span-4 items-center'>
       <div className='mr-5 w-[200px]'>
-        <CustomInput name='name' placeholder='Имя' />
+        <input
+          type='text'
+          name='name'
+          placeholder='Имя'
+          className='text-sm rounded-lg block w-full py-2 px-3 bg-neutral-800 placeholder-gray-400 text-white'
+        />
       </div>
       <div className='mr-5 w-[200px]'>
-        <CustomInput name='category' placeholder='Жанр' />
+        <input
+          type='text'
+          name='category'
+          placeholder='Жанр'
+          className='text-sm rounded-lg block w-full py-2 px-3 bg-neutral-800 placeholder-gray-400 text-white'
+        />
       </div>
       <div className='mr-5 w-[100px]'>
-        <CustomInput name='year' placeholder='Год' />
+        <input
+          type='text'
+          name='year'
+          placeholder='Год'
+          className='text-sm rounded-lg block w-full py-2 px-3 bg-neutral-800 placeholder-gray-400 text-white'
+        />
       </div>
       <button
         type='submit'
