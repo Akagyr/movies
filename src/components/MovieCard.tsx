2a29098ed8c60d4e50afc57fb994c2b3eaee4b05
Alex <casha.mironyuk.2001@gmail.com>
2024-07-11 17:11:13 +0300

    Fixed bug with icons see later and favourite

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,24 +1,35 @@
+'use client';
+
 import Link from 'next/link';
 import Image from 'next/image';
 import { Movie } from '@/lib/types';
 import RateStars from './RateStars';
 import FavouriteIcon from './FavouriteIcon';
 import SeeLaterIcon from './SeeLaterIcon';
+import { usePathname } from 'next/navigation';
 
 export default function MovieCard({
   movie,
   updateFavourites,
-  updateSeeLater
+  updateSeeLater,
 }: {
   movie: Movie;
   updateFavourites?: (movieSlug: string) => void;
   updateSeeLater?: (movieSlug: string) => void;
 }) {
+  const pathname = usePathname();
+  const isOnFavouritesPage = pathname === '/favourites';
+  const isOnSeeLaterPage = pathname === '/seeLater';
+
   return (
     <div className='w-[250px] lg:w-[270px] h-full bg-[#141313] lg:hover:scale-[1.01] lg:hover:transition lg:hover:duration-500 lg:hover:ease-out rounded-lg shadow relative'>
       <div className='absolute top-0 right-0 flex gap-[7px] bg-[#141313]/85 px-[7px] py-[5px] rounded-bl-lg z-1'>
-        <FavouriteIcon movieSlug={movie.slug} updateFavourites={updateFavourites} />
-        <SeeLaterIcon movieSlug={movie.slug} updateSeeLater={updateSeeLater} />
+        {!isOnSeeLaterPage && (
+          <FavouriteIcon movieSlug={movie.slug} updateFavourites={updateFavourites} />
+        )}
+        {!isOnFavouritesPage && (
+          <SeeLaterIcon movieSlug={movie.slug} updateSeeLater={updateSeeLater} />
+        )}
       </div>
       <Link href={`${movie.slug}`}>
         <Image
