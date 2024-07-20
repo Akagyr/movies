0be88e3d8bc9b04cbe6fb44ffdeea84c35e1faa8
Alex <casha.mironyuk.2001@gmail.com>
2024-07-20 15:45:14 +0300

    Disabled cashing in search pasge

--- src/app/(pages)/search/page.tsx
+++ src/app/(pages)/search/page.tsx
@@ -5,6 +5,8 @@ import MovieCard from '@/components/MovieCard';
 import { getDBCollection } from '@/database/databaseServices';
 import { redirect } from 'next/navigation';
 
+export const revalidate = 0;
+
 export default async function Search({
   searchParams,
 }: {
